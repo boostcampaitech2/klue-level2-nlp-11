45e5c8999ea4a8cc43011c8b458d6c49e83b0bd7
Amenpark <puzzlistpam@gmail.com>
2021-09-30 09:55:06 +0000

    get random_seed and shuffled

--- code/inference.py
+++ code/inference.py
@@ -24,7 +24,7 @@ def inference(model, tokenized_sent, device):
       outputs = model(
           input_ids=data['input_ids'].to(device),
           attention_mask=data['attention_mask'].to(device),
-          token_type_ids=data['token_type_ids'].to(device)
+          #token_type_ids=data['token_type_ids'].to(device)
           )
     logits = outputs[0]
     prob = F.softmax(logits, dim=-1).detach().cpu().numpy()
