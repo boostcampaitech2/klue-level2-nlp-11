af5cab138e484e6170866544f486e0c922e425a5
MaruchanPark <shaild098@naver.com>
2021-10-05 08:10:22 +0000

    ignore test 2

--- code/run_train.sh
+++ code/run_train.sh
@@ -11,3 +11,5 @@ python train.py --model_name pre_klue/bert-base --run_name 10/4_$NAME --train_ba
 #python train.py --model_name pre_klue/bert-base --run_name 10/4_$NAME --train_batch_size 64 --eval_batch_size 32 \
 #        --num_train_epochs 10 --eval_steps 500 --save_steps 500 --learning_rate 5e-5 --warmup_steps 2000 \
 #       	--weight_decay 0.01 --opt_loss f1 --early_stopping false \
+
+#       	--weight_decay 0.01 --opt_loss f1 --early_stopping false \
